18d8c2d0b748d3bdabbdcb3b5ffbbd8cc9e02f27
miritzelichonok1 <miritz@startapp.com>
2022-02-05 15:45:38 +0200

    finalizing purchase process

--- pages/payment_page/payment_page.py
+++ pages/payment_page/payment_page.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request, redirect
 from utilities.db.DTO.Customer import get_customer_by_email
+import utilities.db.DAO.OrdersDAO as OrdersDAO
 
 # about blueprint definition
 payment_page = Blueprint('payment_page', __name__, static_folder='static', static_url_path='/payment_page', template_folder='templates')
@@ -8,4 +9,17 @@ payment_page = Blueprint('payment_page', __name__, static_folder='static', stati
 # Routes
 @payment_page.route('/payment_page')
 def index():
-    return render_template('payment_page.html', customer=get_customer_by_email(session['user']))
+    customer = get_customer_by_email(session['user'])
+    ticketType = request.args.to_dict(flat=False).get('product')[0]
+    return render_template('payment_page.html', customer=customer, ticketType=ticketType)
+
+@payment_page.route('/payment_page/make_payment', methods=['POST'])
+def make_payment():
+    ticketType = request.form['ticketType']
+    orderId = OrdersDAO.OrdersDAO().get_new_orderId()
+    ticketInfo = OrdersDAO.OrdersDAO().get_ticketType_info(ticketType)
+    numOfEntries = ticketInfo[0]
+    price = ticketInfo[1]
+    if OrdersDAO.OrdersDAO().create_order(orderId, session['user']):
+        OrdersDAO.OrdersDAO().create_user_entry_ticket(session['user'], orderId, numOfEntries, ticketType, price)
+    return redirect('/')
--- pages/price_list/price_list.py
+++ pages/price_list/price_list.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+import utilities.db.DAO.workoutsDAO as WorkoutsDAO
 
 # about blueprint definition
 price_list = Blueprint('price_list', __name__, static_folder='static', static_url_path='/price_list', template_folder='templates')
@@ -7,4 +8,5 @@ price_list = Blueprint('price_list', __name__, static_folder='static', static_ur
 # Routes
 @price_list.route('/price_list')
 def index():
-    return render_template('price_list.html')
+    return render_template('price_list.html', workouts=WorkoutsDAO.WorkoutsDAO().get_workout_price_list())
+
