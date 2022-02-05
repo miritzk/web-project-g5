09ec4c4334fb4cceb92cbfab4a9e6996d85b1098
miritzelichonok1 <miritz@startapp.com>
2022-02-05 11:02:58 +0200

    start filling payment page

--- pages/payment_page/payment_page.py
+++ pages/payment_page/payment_page.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from utilities.db.DTO.Customer import get_customer_by_email
 
 # about blueprint definition
 payment_page = Blueprint('payment_page', __name__, static_folder='static', static_url_path='/payment_page', template_folder='templates')
@@ -7,4 +8,4 @@ payment_page = Blueprint('payment_page', __name__, static_folder='static', stati
 # Routes
 @payment_page.route('/payment_page')
 def index():
-    return render_template('payment_page.html')
+    return render_template('payment_page.html', customer=get_customer_by_email(session['user']))
